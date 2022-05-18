fe1112338150a713009ee74ceb0968244b49cb90
TKadyear <tamarakadyear@gmail.com>
2022-05-18 19:34:53 +0200

    Reestructure the login_spec and remove all the empty test

--- cypress/login/relocation-for-privates-routes_spec.js
+++
@@ -1,9 +0,0 @@
-describe('Test relocation to login If the user is not authenticated ', () => {
-  // beforeEach(() => {
-  //   cy.visit('/')
-  // })
-  it('Home', () => {
-    cy.url().should('include', '/login');
-  })
-
-})
