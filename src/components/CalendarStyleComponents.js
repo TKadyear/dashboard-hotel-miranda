876c389996f5e2281c4d0bad18f636877d026020
TKadyear <tamarakadyear@gmail.com>
2022-05-21 19:54:31 +0200

    Placed correctly all the days of the calendar

--- src/components/CalendarStyleComponents.js
+++ src/components/CalendarStyleComponents.js
@@ -8,17 +8,19 @@ export const FullCalendarWrapper = styled.div`
   background: ${props => props.theme.colors.white};
   font-family: ${props => props.theme.font};
   box-shadow:${props => props.theme.boxShadow.dirY} ;
-
   .fc-theme-standard .fc-scrollgrid, .fc-theme-standard th, .fc-scrollgrid td{
      border:0;
   }
+
   th{
     font-weight: 400;
     color: ${props => props.theme.colors.light};
   }
+
   .fc .fc-toolbar-chunk:first-child:before{
     content:"Recent Booking Schedule";
   }
+
   .fc .fc-toolbar-chunk:last-child{
     div{
       display:grid;
@@ -26,13 +28,16 @@ export const FullCalendarWrapper = styled.div`
       gap: 1rem;
     }
   }
+
   .fc-toolbar-title, .fc .fc-toolbar-chunk:first-child:before{
     font-weight: 500;
     font-size: 1.125rem;
   }
+
   .fc .fc-button{
     padding: 0;
   }
+
   .fc .fc-button-primary{
     background-color: ${props => props.theme.colors.white};
     border: 0;
@@ -49,5 +54,26 @@ export const FullCalendarWrapper = styled.div`
     :focus{
       border:0;
     }
+  }
+
+  .fc .fc-daygrid-day-frame,
+  .fc .fc-daygrid-day-top {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+/*Clear the :before */
+  .fc-daygrid-day-frame:before, .fc-daygrid-day-events:before, .fc-daygrid-event-harness:before,
+  .fc-daygrid-day-frame:after, .fc-daygrid-day-events:after, .fc-daygrid-event-harness:after {
+    display: none;
+  }
+  .fc .fc-daygrid-body-unbalanced .fc-daygrid-day-events {
+      display: none;
+  }
+  /* Clear scrollBar style */
+  .fc .fc-scroller-liquid-absolute{
+    /*Because Full Calendar was applying the style = >overflow in the html markup */
+    overflow: hidden!important;
   }
 `;
