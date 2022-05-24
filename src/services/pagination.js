8b56bd888d9aeb0f8259820fd646df097b82d6b1
TKadyear <tamarakadyear@gmail.com>
2022-05-24 12:42:27 +0200

    WIP - Fixed problem with booking slice and the page of bookings
    Because It's better have the split Pagination in the component instead of having the function in the selector

--- src/services/pagination.js
+++ src/services/pagination.js
@@ -1,7 +1,7 @@
 export const splitForPagination = (allItems, itemsPerPage = 10) => {
   let list = [];
   for (let pageNumber = 0; pageNumber < allItems.length; pageNumber += itemsPerPage) {
-    const newList = allItems.slice(pageNumber, pageNumber + itemsPerPage);
+    const newList = [...allItems].slice(pageNumber, pageNumber + itemsPerPage);
     list.push(newList);
   }
   return list;
