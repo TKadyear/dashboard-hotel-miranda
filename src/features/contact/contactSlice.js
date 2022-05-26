c97dc1ca84e17bcee9c372a8bb0d2bcd3570447e
TKadyear <tamarakadyear@gmail.com>
2022-05-27 01:17:03 +0200

    Added the async thunk to Contact Slice

--- src/features/contact/contactSlice.js
+++ src/features/contact/contactSlice.js
@@ -1,17 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { initialState } from "./initialState";
+export const fetchContact = createAsyncThunk("contact/fetchContact", async () => {
+  return new Promise(resolve => setTimeout(resolve(initialState), 0));
+});
 
 export const contactSlice = createSlice({
   name: "contact",
-  initialState: initialState,
+  initialState: [],
   reducers: {
-    createNewReview: (state, action) => {
-      return [...state, action.payload];
+    archiveMessage: (state, action) => {
+      const newState = state.map(room => room.id === action.payload.id ? action.payload : room);
+      return newState;
+    },
+    deleteMessage: (state, action) => {
+      return state.filter(room => room.id != action.payload);
     }
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchContact.fulfilled, (state, action) => {
+        return action.payload;
+      });
   }
 });
-
-// IMPROVE ?
 
 export const allContact = state => [...state.contact].sort((a, b) => {
   const dateA = new Date(a.review.date);
@@ -19,6 +30,7 @@ export const allContact = state => [...state.contact].sort((a, b) => {
   return dateA.getTime() - dateB.getTime();
 });
 
-// export const { addPhoto, removePhoto, editDescription } = myPhotosSlice.actions;
+
+export const { archiveMessage, deleteMessage } = contactSlice.actions;
 
 export default contactSlice.reducer;
