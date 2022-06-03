076b2589e736b852ec1f0e9d9cca89fe6ff36334
TKadyear <tamarakadyear@gmail.com>
2022-06-03 08:00:33 +0200

    WIP | Done chart but It doesn't have a legend and the events

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -9,9 +9,9 @@ const SvgStyled = styled.svg`
 
 
 export const BarChart = () => {
-  const margin = { top: 30, right: 30, bottom: 70, left: 60 };
-  const width = 500;
-  const height = 400;
+  const margin = { top: 30, right: 30, bottom: 70, left: 70 };
+  const width = 550;
+  const height = 500;
 
   useEffect(() => {
     // TO ASK Tendría que hacer un map y que el valor dependa de los datos que llegan pero añadiendole un poco más al rango
@@ -22,83 +22,67 @@ export const BarChart = () => {
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    //Custom Axis
-    // Add X axis
     const xScale = scaleBand()
-      .range([0, width])
-      .domain(data.map(value => value.day))
+      .range([0, width - margin.left - margin.right])
       .padding(0.3);
+    const xScaleSecondary = scaleBand()
+      .domain(["amountSales", "occupancyPercentage"])
+      .range([0, xScale.bandwidth()]);
+    const yScale = scaleLinear()
+      .range([height - margin.top - margin.bottom, 0])
+      .domain([0, maxSales]);
+    const yScaleRight = scaleLinear()
+      .range([height - margin.top - margin.bottom, 0])
+      .domain([0, 100]);
 
-    const axisX = axisBottom(xScale).ticks(10);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(axisX)
-      .selectAll("text")
-      .attr("transform", "translate(-10,0)")
-      .attr("width", 50)
-      .style("text-anchor", "start");
-    const xScaleSecondary = scaleBand().domain(["amountSales", "ocuppancyPercetange"]);
+    const xAxis = axisBottom(xScale).tickSizeOuter(0);
+    const yAxis = axisLeft(yScale).ticks(10).tickSizeOuter(0);
+    const yAxis2 = axisRight(yScaleRight).ticks(10).tickSizeOuter(0);
 
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(axisX);
+    xScale.domain(data.map(d => d.day));
 
-    // Add Y axis
-    const yRightScale = scaleLinear()
-      .domain([0, 100])
-      .range([height, 0]);
-
-    const yRightAxis = axisRight(yRightScale).ticks(10).tickSizeOuter(0);
-    svg.append("g")
-      .attr("transform", "translate(" + width + ",0)")
-      .call(yRightAxis)
-      .selectAll("text")
-      .style("text-anchor", "start");
-
-
-    const yLeftScale = scaleLinear()
-      .domain([0, maxSales])
-      .range([height, 0]);
-
-    const yLeftAxis = axisLeft(yLeftScale).ticks(10).tickSizeOuter(0);
-    svg.append("g")
-      .call(yLeftAxis)
-      .selectAll("text")
-      .style("text-anchor", "end");
-
-    //Weekday
-    let weekday = svg.selectAll("weekday")
+    const weekday = svg.selectAll(".weekday")
       .data(data)
       .enter()
-      .attr("transform", data => `translate(${xScale(data.weekday)},0)`)
-      .append("g");
+      .append("g")
+      .attr("transform", d => `translate(${xScale(d.day)},0)`);
 
-    // Add Amount Sales bar
-    weekday.selectAll("amount-sales")
-      .data(data => [data])
+    /// Add bars
+    weekday.selectAll("amountSales")
+      .data(d => [d])
       .enter()
       .append("rect")
       .style("fill", "#135846")
       .attr("x", xScaleSecondary("amountSales"))
-      .attr("y", value => yLeftScale(value.amountSales))
-      .attr("width", xScale.bandwidth())
+      .attr("y", d => yScale(d.amountSales))
+      .attr("width", 15)
       .attr("height", d => {
-        return height - margin.top - margin.bottom - d.amountSales;
+        return height - margin.top - margin.bottom - yScale(d.amountSales);
       });
 
-    // //Add occupancyPercentage
-    // weekday.selectAll("occupancy-percentage")
-    //   .data(data => [data])
-    //   .enter()
-    //   .append("rect")
-    //   .attr("class", "bar occupancy")
-    //   .style("fill", "#e23428")
-    //   .attr("x", xSecondary("occupancy"))
-    //   .attr("y", d => yRight(d.occupancyPercentage))
-    //   .attr("width", x.bandwidth())
-    //   .attr("height", d => {
-    //     return height - margin.top - margin.bottom - d.occupancyPercentage;
-    //   });
+    weekday.selectAll("ocuppancyPercentage")
+      .data(d => [d])
+      .enter()
+      .append("rect")
+      .style("fill", "#e23428")
+      .attr("x", 15)
+      .attr("y", d => yScaleRight(d.occupancyPercentage))
+      .attr("width", 15)
+      .attr("height", d => {
+        return height - margin.top - margin.bottom - yScaleRight(d.occupancyPercentage);
+      });
+
+    // Add the X Axis
+    svg.append("g")
+      .attr("transform", `translate(0,${height - margin.top - margin.bottom})`)
+      .call(xAxis);
+    // Add the Y Axis
+    svg.append("g")
+      .call(yAxis);
+    svg.append("g")
+      .attr("transform", `translate(${width - margin.right - margin.left}, 0)`)
+      .call(yAxis2);
+
 
   }, []);
   return (
