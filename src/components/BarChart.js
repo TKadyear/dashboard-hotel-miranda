c64b3b34c022e8db1e73a333adca31d282a8855e
TKadyear <tamarakadyear@gmail.com>
2022-06-03 06:36:32 +0200

    WIP

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,77 +1,99 @@
 import styled from "styled-components";
 import { select, scaleBand, axisBottom, axisRight, axisLeft, scaleLinear } from "d3";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
+import { mockData as data } from "./dataChart";
 const SvgStyled = styled.svg`
-  border: 1px solid red;
   width: 100%;
   height: 100%;
 `;
 
 
-// set the dimensions and margins of the graph
-
 export const BarChart = () => {
-  const svgRef = useRef();
-  // const wrapperRef = useRef();
-  const data = [12, 5, 6, 6, 9, 10];
-  const axisX = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const margin = { top: 30, right: 30, bottom: 70, left: 60 };
   const width = 460 - margin.left - margin.right;
   const height = 400 - margin.top - margin.bottom;
 
   useEffect(() => {
-    const svg = select(svgRef.current)
+    const maxSales = 6000;
+    const svg = select("#chart")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    //Custom Axis
     // Add X axis
     const x = scaleBand()
       .range([0, width])
-      .domain(axisX)
-      .padding(0.2);
+      .domain(data.map(value => value.day))
+      .padding(0.3);
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(axisBottom(x))
       .selectAll("text")
       .attr("transform", "translate(-10,0)")
       .attr("width", 50)
-      .style("text-anchor", "end");
+      .style("text-anchor", "start");
+    const xSecondary = scaleBand(); //For the other bar chart
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(axisBottom(xSecondary));
 
     // Add Y axis
     const yRight = scaleLinear()
       .domain([0, 100])
       .range([height, 0]);
     svg.append("g")
+      .attr("transform", "translate(" + width + ",0)")
       .call(axisRight(yRight))
       .selectAll("text")
-      .attr("transform", "translate(" + width + ",0)")
       .style("text-anchor", "start");
 
 
     const yLeft = scaleLinear()
-      .domain([0, 1200])
+      .domain([0, maxSales])
       .range([height, 0]);
     svg.append("g")
       .call(axisLeft(yLeft))
       .selectAll("text")
       .style("text-anchor", "end");
-    //Add Bart
-    svg
-      .selectAll("rect")
+
+    //Weekday
+    let weekday = svg.selectAll("weekday")
       .data(data)
+      .enter().append("g")
+      .attr("class", "week_day");
+
+    // Add Amount Sales bar
+    weekday.selectAll("amount-sales")
+      .data(data => [data])
       .enter()
       .append("rect")
-      .attr("x", 15) //This property make the space between each rect
-      .attr("y", value => value) // 150 Represent the total height of the svg, so If you rest the value of the bar/rect make
+      .style("fill", "#135846")
+      .attr("x", value => value.amountSales + 10)
+      .attr("y", value => value.amountSales - yLeft)
       .attr("width", x.bandwidth())
-      .attr("height", (value) => height - value)
-      .attr("fill", "green");
+      .attr("height", d => {
+        return height - margin.top - margin.bottom - d.amountSales;
+      });
+
+    //Add occupancyPercentage
+    weekday.selectAll("occupancy-percentage")
+      .data(data => [data])
+      .enter()
+      .append("rect")
+      .attr("class", "bar occupancy")
+      .style("fill", "#e23428")
+      .attr("x", xSecondary("occupancy"))
+      .attr("y", d => yRight(d.occupancyPercentage))
+      .attr("width", x.bandwidth())
+      .attr("height", d => {
+        return height - margin.top - margin.bottom - d.occupancyPercentage;
+      });
 
   }, []);
   return (
-    <SvgStyled margin={margin} ref={svgRef}>
+    <SvgStyled margin={margin} id="chart">
     </SvgStyled>
   );
 };
---
+++ src/components/dataChart.js
@@ -0,0 +1,42 @@
+export const mockData = [
+  {
+    "day": "Mon",
+    "amountSales": 2025,
+    "occupancyPercentage": 19
+  },
+  {
+    "day": "Tue",
+    "amountSales": 650,
+    "occupancyPercentage": 67
+  },
+  {
+    "day": "Wed",
+    "amountSales": 540,
+    "occupancyPercentage": 10
+  },
+  {
+    "day": "Thu",
+    "amountSales": 4307,
+    "occupancyPercentage": 23
+  },
+  {
+    "day": "Fri",
+    "amountSales": 2325,
+    "occupancyPercentage": 78
+  },
+  {
+    "day": "Sat",
+    "amountSales": 5468,
+    "occupancyPercentage": 90
+  },
+  {
+    "day": "Sun",
+    "amountSales": 5478,
+    "occupancyPercentage": 25
+  },
+];
+
+// new Date().toLocaleDateString("en-US", { weekday: "short" })
+// const dataChart = (data) =>{
+//   const getSales = data
+// }
